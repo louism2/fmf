e3e415613ff3ab8f62481ca79c5e72932c1ab048
lmancini <lmancini@g2llc.com>
2017-01-10 10:48:19 -0800

    added room and roomList to app module

--- app/assets/javascripts/app.config.js
+++ app/assets/javascripts/app.config.js
@@ -1,18 +1,9 @@
-angular.module('roomList', ['ngResource']);
-
-angular.module('roomList').component('roomList', {
-    templateUrl: 'room-list/room-list.template.html',
-    controller: [function RoomListController(){
-          this.rooms = []
-        }
-    ]
-  });
-
 angular.module('fmfApp', [
   'templates',
   'ngAnimate',
   'ngRoute',
-  'roomList'
+  'roomList',
+  'room'
 ]);
 
 angular.module('fmfApp').config(['$locationProvider' ,'$routeProvider',
