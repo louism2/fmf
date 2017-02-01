35ab5d5e5aa8b2cb946ad707856bf8b047044ed1
lmancini <lmancini@g2llc.com>
2017-01-31 17:47:50 -0800

    create getTemplate helper method

--- spec/javascripts/helpers/angular_helpers.js
+++ spec/javascripts/helpers/angular_helpers.js
@@ -1,1 +1,26 @@
 //= require angular-mocks
+
+
+function getTemplate(path_from_templates_dir){
+  var t = inject(function($templateCache){
+    var template = $templateCache.get(path_from_templates_dir)
+
+    if(template){
+      return template;
+    }
+
+    var req = new XMLHttpRequest();
+    req.onload = function() {
+        template = this.responseText;
+    };
+    // Note that the relative path may be different from your unit test HTML file.
+    // Using `false` as the third parameter to open() makes the operation synchronous.
+    // Gentle reminder that boolean parameters are not the best API choice.
+    req.open("get", "../../../assets/templates/"+path_from_templates_dir, false);
+    req.send();
+    $templateCache.put(path_from_templates_dir, template);
+    return $templateCache.get(path_from_templates_dir)
+  });
+
+  return t;
+}
